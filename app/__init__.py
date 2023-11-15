c6c7a8c59fed00c67e493440428235d151b7efed
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2023-11-15 14:36:51 -0300

    fix:

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,21 @@
 # app/__init__.py
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+database = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    
+    bcrypt = Bcrypt(app)
+    login_manager = LoginManager(app)
+
+    database.init_app(app)  # Inicializa o Flask-SQLAlchemy
 
     from .routes import main
     app.register_blueprint(main)
--- config.py
+++ config.py
@@ -1,2 +1,6 @@
+import os
+
 class Config:
     SECRET_KEY = 'chave_secreta_aqui'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.getcwd(), 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
